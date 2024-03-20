3f173ffc14eaa8d7fb9d1a4578af537c3543e287
Ranjeetvelocity13 <ranjeetvelocity13@gmail.com>
2024-03-20 20:31:06 +0530

    First commit

---
+++ src/Sample/Demo.java
@@ -0,0 +1,21 @@
+package Sample;
+
+public class Demo { // Class body
+	
+	public static void main(String []args) { //Main Method body
+	//	main ctrl+space
+		
+	// Compiler is a translator that convert the high level language. to machine language.
+		
+		System.out.println("Rahul ");  // syso ctrl+space  
+		
+		System.out.println("Patil");
+				
+	}
+
+}
+
+
+
+
+
